c856318a6dc6d8483964ecb480802a48e056418c
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-01-23 15:47:14 -0300

    create a component for the table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./traive-logo-green-tm.svg";
 import "./App.css";
+import CreditRequestsTable from "./CreditRequestsTable";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo mb-3" alt="Traive™" />
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <CreditRequestsTable />
       </header>
     </div>
   );
