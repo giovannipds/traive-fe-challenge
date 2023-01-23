af1845a09ec4598ed22739399b450a6d00a2adae
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-01-23 15:57:21 -0300

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./traive-logo-green-tm.svg";
 import "./App.css";
 import CreditRequestsTable from "./CreditRequestsTable";
 
 const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3004/credit-requests");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
