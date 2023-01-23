895852cd2044c941ab280c6dcaeb9b65b4be3147
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-01-23 15:49:56 -0300

    columns

--- src/CreditRequestsTable.tsx
+++ src/CreditRequestsTable.tsx
@@ -5,25 +5,29 @@ function CreditRequestsTable() {
     <table className="table-auto">
       <thead>
         <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th>Year</th>
+          <th>Season</th>
+          <th>Amount Requested</th>
+          <th>Purpose</th>
+          <th>Due Date</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
           <td>Malcolm Lockyer</td>
+          <td>Lorem</td>
           <td>1961</td>
         </tr>
         <tr>
           <td>Witchy Woman</td>
           <td>The Eagles</td>
+          <td>Ipsum</td>
           <td>1972</td>
         </tr>
         <tr>
           <td>Shining Star</td>
           <td>Earth, Wind, and Fire</td>
+          <td>Dolor</td>
           <td>1975</td>
         </tr>
       </tbody>
